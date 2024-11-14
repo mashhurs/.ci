7b4e73d7c2b862b2da82c958f4f7e47fd93925d7
donoghuc <cas.donoghue@gmail.com>
2024-11-14 08:11:21 -0800

    Update variable name to be more descriptive
    This commit updates the script to be more descriptive with the naming of a
    variable that indicates the stack version. The stack version may be an alias
    when provided to this script. Using variable names to track when we are dealing
    with a potential alias (vs when we have normalized) helps with maintainability.

--- docker-setup.sh
+++ docker-setup.sh
@@ -6,13 +6,13 @@ set -e
 
 pull_docker_snapshot() {
   project="${1?project name required}"
-  stack_version="${2?stack version required}"
+  stack_version_alias="${2?stack version alias required}"
   local docker_image="docker.elastic.co/${project}/${project}${DISTRIBUTION_SUFFIX}:${ELASTIC_STACK_VERSION}"
   echo "Pulling $docker_image"
   if docker pull "$docker_image" ; then
     echo "docker pull successful"
   else
-    case $stack_version in
+    case $stack_version_alias in
       "8.previous"|"8.current"|"8.next")
         exit 1
         ;;
@@ -31,8 +31,8 @@ if [ -z "${ELASTIC_STACK_VERSION}" ]; then
     exit 1
 fi
 
-# save the original unmodified string to pass to pull_docker_snapshot
-ELASTIC_STACK_VERSION_ARG="$ELASTIC_STACK_VERSION"
+# The ELASTIC_STACK_VERSION may be an alias, save the original before translating it
+ELASTIC_STACK_VERSION_ALIAS="$ELASTIC_STACK_VERSION"
 
 echo "Fetching versions from $VERSION_URL"
 VERSIONS=$(curl -s $VERSION_URL)
@@ -64,7 +64,7 @@ export DISTRIBUTION_SUFFIX
 echo "Testing against version: $ELASTIC_STACK_VERSION (distribution: ${DISTRIBUTION:-"default"})"
 
 if [[ "$ELASTIC_STACK_VERSION" = *"-SNAPSHOT" ]]; then
-    pull_docker_snapshot "logstash" $ELASTIC_STACK_VERSION_ARG
+    pull_docker_snapshot "logstash" $ELASTIC_STACK_VERSION_ALIAS
 fi
 
 if [ -f Gemfile.lock ]; then
